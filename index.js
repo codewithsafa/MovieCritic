c3267b46c3dee2b2ebd6c5c3fccb008f6e8db734
safa <safaalam19161@gmail.com>
2026-07-14 00:04:59 +0530

    adding error handling code

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ const db = new pg.Client({
     },
 });
 
-db.connect();
+db.connect()
+  .then(() => console.log("Connected to Neon"))
+  .catch(err => console.error("Database connection failed:", err));
+
+
 app.set('view engine', 'ejs');
 
 
@@ -103,8 +107,9 @@ app.get("/", async (req, res) =>{
     })
 // })
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
-}
-)
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
